150736f4ac1ae53230f45add67c113aa7addfff0
NamLe179 <ldnam179@gmail.com>
2025-11-12 21:59:50 +0700

    update range and minor fix for better experience

--- Assets/GameFolders/Scripts/Concretes/Controllers/EndController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/EndController.cs
@@ -1,7 +1,9 @@
 using Managers;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Controllers
 {
@@ -9,7 +11,20 @@ namespace Controllers
     {
         private void OnTriggerEnter2D(Collider2D collision) //Kích hoạt khi mục tiêu đi vào collider của đối tượng
         {
-            GameManager.Instance.EndGame();
+            // GameManager.Instance.EndGame();
+            Scene currentScene = SceneManager.GetActiveScene();
+            String sceneName = currentScene.name;
+            if (sceneName == "Level 1(Tutorial)")
+            {
+                SceneManager.LoadScene("Level 2");
+            }
+            else if (sceneName == "Level 2")
+            {
+                SceneManager.LoadScene("Level 3");
+            }
+            else GameManager.Instance.EndGame();
+            // Đoạn sau sẽ xử lý load lại về game 3d
+
            
         }
     }
